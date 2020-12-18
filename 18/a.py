fc70432cb5964c8cdf5a2b42a19e40734c3ff31a
Rick James <rkj@fb.com>
2020-12-17 22:02:21 -0800

    day 18 part 2

--- 18/a.py
+++ 18/a.py
@@ -10,9 +10,12 @@ lines = data.readlines()
 
 # no brackets eval
 def simple(line):
-    #print(line)
+    #print("simple", line)
     if "(" in line or ")" in line:
         print("simpleunexpected", line)
+
+    if "+" in line and "*" in line:
+        print("simpleunexpected2", line)
 
     result = 0
 
@@ -41,7 +44,33 @@ def simple(line):
         else:
             line = str(result) + extra
 
+    #print("simple", result, "=", line)
     return result
+
+# destroy brackets
+def plusfirst(line):
+    result = 0
+
+    while True:
+        m = re.search("^(.* )(\d+ \+ \d+)(.*)$", line)
+        if m is None:
+            result = simple(line)
+            break
+
+        left = m.group(1)
+        inner = m.group(2)
+        right = m.group(3)
+
+        result = simple(inner)
+
+        if left == "" and right == "":
+            break
+        else:
+            line = left + str(result) + right
+
+    #print("plusfirst", result, "=", line)
+    return result
+
 
 # destroy brackets
 def bracket(line):
@@ -50,7 +79,7 @@ def bracket(line):
     while True:
         m = re.search("^(.*)\(([^)]*)\)(.*)$", line)
         if m is None:
-            result = simple(line)
+            result = plusfirst(line)
             break
 
         left = m.group(1)
@@ -71,6 +100,7 @@ for line in lines:
     if line == "\n":
         continue
     r = bracket(line)
+    print(r)
     s += r
 
 print("sum", s)
